d9fd06de140db9a5ca42f3f74f3199270c24e0c8
jupi <leivadiazjulio@gmail.com>
2017-07-18 10:25:14 +0200

    Set-up project

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container" id="app">
+
+
+    <SourceSelection v-on:sourceChanged="sourceChanged"></SourceSelection>
+    <Newslist v-bind:source="source"></Newslist>
+
+    <div class="footer">
+      <p>&copy <a href="http://jleiva.es" target="_blank">jleiva.es</a> </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import Newslist from './components/Newslist'
+import SourceSelection from './components/SourceSelection'
+
+export default {
+  name: 'app',
+  components: {
+    Newslist,
+    SourceSelection
+  },
+  data () {
+    return {
+      source: ""
+    }
+  },
+  methods: {
+    sourceChanged: function (source) {
+      this.source = source;
+    }
+  }
+}
+</script>
+
+<style>
+  #app {
+    padding-top: 20px
+  }
+</style>
